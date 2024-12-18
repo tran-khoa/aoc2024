7163225600f35a2b2cf54c5a13eff30704d0ae5a
Viet Anh Khoa Tran <v.tran@fz-juelich.de>
2024-12-19 00:50:05 +0700

    it works but damn

--- day17/rust/src/main.rs
+++ day17/rust/src/main.rs
@@ -1,5 +1,6 @@
 use regex::Regex;
 
+#[derive(Clone)]
 struct Computer {
     a: u64,
     b: u64,
@@ -71,69 +72,126 @@ impl Computer {
     fn jump_instruction(&mut self, pos: usize) {
         self.i_ptr = pos;
     }
+    fn reset(&mut self) {
+        self.i_ptr = 0;
+        self.a = 0;
+        self.b = 0;
+        self.c = 0;
+    }
+
+    fn compute_next_output(&mut self) -> Option<u64> {
+        loop {
+            let opcode = match self.current_instruction() {
+                Some(opcode) => opcode,
+                None => {
+                    return None;
+                }
+            };
+
+            match opcode {
+                0 => {
+                    let denom = self.fetch_combo_operand();
+                    self.a /= 2u64.pow(denom as u32);
+                    self.next_instruction();
+                }
+                1 => {
+                    self.b = self.b ^ self.fetch_operand();
+                    self.next_instruction();
+                }
+                2 => {
+                    self.b = self.fetch_combo_operand() % 8;
+                    self.next_instruction();
+                }
+                3 => {
+                    if self.a == 0 {
+                        self.next_instruction();
+                        continue;
+                    }
+                    self.jump_instruction(self.fetch_operand() as usize);
+                }
+                4 => {
+                    self.b ^= self.c;
+                    self.next_instruction();
+                }
+                5 => {
+                    let output = self.fetch_combo_operand() % 8;
+                    self.next_instruction();
+                    return Some(output);
+                }
+                6 => {
+                    let denom = self.fetch_combo_operand();
+                    self.b = self.a / 2u64.pow(denom as u32);
+                    self.next_instruction();
+                }
+                7 => {
+                    let denom = self.fetch_combo_operand();
+                    self.c = self.a / 2u64.pow(denom as u32);
+                    self.next_instruction();
+                }
+                _ => panic!("Invalid instruction {}", opcode),
+            }
+        }
+    }
+
+    fn compute(&mut self) -> Vec<u64> {
+        let mut outputs: Vec<u64> = Vec::new();
+        while let Some(output) = self.compute_next_output() {
+            outputs.push(output);
+        }
+        outputs
+    }
+}
+
+fn part1(comp: &Computer) -> String {
+    let mut comp = comp.clone();
+    comp.compute()
+        .iter()
+        .map(|x| x.to_string())
+        .collect::<Vec<_>>()
+        .join(",")
+}
+
+fn part2(comp: &Computer) -> u64 {
+    let mut comp = comp.clone();
+
+    // try to reproduce 0 as output
+    let mut search_candidates: Vec<u64> = Vec::new();
+    for i in 0..=0b1111111111 {
+        // 10 bits
+        comp.reset();
+        comp.a = i;
+        if comp.compute_next_output().unwrap() == *comp.program.first().unwrap() {
+            search_candidates.push(i);
+        }
+    }
+
+    let program = comp.program.clone();
+    for (idx, &target) in program.iter().enumerate().skip(1) {
+        let mut new_search_candidates: Vec<u64> = Vec::new();
+        search_candidates.iter().for_each(|cand| {
+            for new_first3 in 0..=0b111 {
+                let partial_cand = ((new_first3 as u64) << 7) | cand >> (3 * idx);
+                comp.reset();
+                comp.a = partial_cand;
+                if comp.compute_next_output().unwrap() == target {
+                    new_search_candidates.push(partial_cand << (3 * idx) | cand & ((1 << (3 * idx)) - 1));
+                }
+            }
+        });
+        search_candidates = new_search_candidates;
+        if search_candidates.len() == 0 {
+            println!("No candidates found for {}", target);
+            break;
+        }
+    }
+    *search_candidates.iter().min().unwrap()
 }
 
 fn main() {
     let input_str = std::fs::read_to_string("../inputs.txt").unwrap();
-    let mut comp = Computer::from_string(input_str);
+    let comp = Computer::from_string(input_str);
 
-    let mut outputs: Vec<u64> = Vec::new();
+    println!("Part 1: {}", part1(&comp));
+    println!("Part 2: {}", part2(&comp));
 
-    loop {
-        let opcode = match comp.current_instruction() {
-            Some(opcode) => opcode,
-            None => break,
-        };
-
-        match opcode {
-            0 => {
-                let denom = comp.fetch_combo_operand();
-                comp.a /= 2u64.pow(denom as u32);
-                comp.next_instruction();
-            }
-            1 => {
-                comp.b = comp.b ^ comp.fetch_operand();
-                comp.next_instruction();
-            }
-            2 => {
-                comp.b = comp.fetch_combo_operand() % 8;
-                comp.next_instruction();
-            }
-            3 => {
-                if comp.a == 0 {
-                    comp.next_instruction();
-                    continue;
-                }
-                comp.jump_instruction(comp.fetch_operand() as usize);
-            }
-            4 => {
-                comp.b ^= comp.c;
-                comp.next_instruction();
-            }
-            5 => {
-                outputs.push(comp.fetch_combo_operand() % 8);
-                comp.next_instruction();
-            }
-            6 => {
-                let denom = comp.fetch_combo_operand();
-                comp.b = comp.a / 2u64.pow(denom as u32);
-                comp.next_instruction();
-            }
-            7 => {
-                let denom = comp.fetch_combo_operand();
-                comp.c = comp.a / 2u64.pow(denom as u32);
-                comp.next_instruction();
-            }
-            _ => panic!("Invalid instruction {}", opcode),
-        }
-    }
-
-    println!(
-        "Part 1: {}",
-        outputs
-            .iter()
-            .map(|x| x.to_string())
-            .collect::<Vec<_>>()
-            .join(",")
-    );
 }
