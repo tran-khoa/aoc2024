8f47c71ba65dbdcbf0427bb9a7c3f6041082955d
Viet Anh Khoa Tran <v.tran@fz-juelich.de>
2024-12-25 21:49:46 +0700

    .

--- day22/rust/src/main.rs
+++ day22/rust/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashMap, VecDeque};
+
 fn evolve(number: i64) -> i64 {
     let number = ((number * 64) ^ number) % 16777216;
     let number = ((number / 32) ^ number) % 16777216;
@@ -5,17 +7,58 @@ fn evolve(number: i64) -> i64 {
     number
 }
 
-fn part1(numbers: &Vec<i64>) -> i64 {
-    numbers
+fn part1(buyers: &Vec<i64>) -> i64 {
+    buyers
         .iter()
-        .map(|n| {
-            let mut n = *n;
+        .map(|buyer_n| {
+            let mut buyer_n = *buyer_n;
             for _ in 0..2000 {
-                n = evolve(n);
+                buyer_n = evolve(buyer_n);
             }
-            return n;
+            return buyer_n;
         })
         .sum()
+}
+
+fn part2(buyers: &Vec<i64>) -> i64 {
+    *buyers
+        .iter()
+        .map(|buyer_n| {
+            let mut buying_sequences: HashMap<(i64, i64, i64, i64), i64> = HashMap::new();
+            let mut prev_price: Option<i64> = None;
+            let mut diffs: VecDeque<i64> = VecDeque::new();
+            let mut buyer_n = *buyer_n;
+            for _ in 0..2000 {
+                let price = buyer_n % 10;
+                if let Some(prev_price) = prev_price {
+                    diffs.push_back(price - prev_price);
+                    if diffs.len() > 4 {
+                        let quadtup = (diffs[0], diffs[1], diffs[2], diffs[3]);
+                        if !buying_sequences.contains_key(&quadtup) {
+                            buying_sequences.insert(quadtup, prev_price);
+                        }
+                        diffs.pop_front();
+                    }
+                }
+                prev_price = Some(price);
+                buyer_n = evolve(buyer_n);
+            }
+            buying_sequences
+        })
+        .fold(
+            HashMap::new(),
+            |mut acc: HashMap<(i64, i64, i64, i64), i64>,
+             buying_sequences: HashMap<(i64, i64, i64, i64), i64>| {
+                for (key, value) in buying_sequences {
+                    *acc.entry(key).or_insert(0) += value;
+                }
+                acc
+            },
+        )
+        .iter()
+        .max_by_key(|(_, &value)| value)
+        .unwrap()
+        .1
 }
 
 fn main() {
@@ -23,4 +66,5 @@ fn main() {
         .map(|file| file.lines().map(|line| line.parse().unwrap()).collect())
         .unwrap();
     println!("Part 1: {}", part1(&numbers));
+    println!("Part 2: {}", part2(&numbers));
 }
